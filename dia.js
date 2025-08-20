7067a5f8acaf1d36d11512ef13e25df368a79271
aldo-misael <75716425+aldo-misael@users.noreply.github.com>
2025-08-19 19:20:22 -0600

    Update dia.js

--- dia.js
+++ dia.js
@@ -10,6 +10,7 @@ const ejerciciosDiv = document.getElementById("ejercicios");
 // Obtener hora actual en formato HH:MM
 function horaActual() {
   const now = new Date();
+  console.log(now);
   return now.getHours().toString().padStart(2, '0') + ":" + now.getMinutes().toString().padStart(2, '0');
 }
 
@@ -137,6 +138,7 @@ document.getElementById("guardar-checks").addEventListener("click", async () => 
 });
 
 cargarPlanHoy();
+
 
 
 
